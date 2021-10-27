967f93a298b963f840108938a6d0de444c0e2752
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2021-10-27 09:28:29 -0400

    handle multiprocessing in Windows build

--- rpdr_converter/rpdr_converter.py
+++ rpdr_converter/rpdr_converter.py
@@ -1,4 +1,4 @@
-from multiprocessing import Queue
+import multiprocessing
 from typing import Callable
 
 from convert_rpdr import convert_rpdr
@@ -8,7 +8,7 @@ from ui import UserInterface
 def process(input_file: str,
             output_file: str,
             append_error: Callable[[str], None],
-            progress_queue: 'Queue[float]') -> None:
+            progress_queue: 'multiprocessing.Queue[float]') -> None:
     try:
         convert_rpdr(input_file, output_file, append_error, progress_queue)
     except Exception as e:
@@ -17,5 +17,9 @@ def process(input_file: str,
 
 
 if __name__ == '__main__':
+    # Required for building on Windows
+    # https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+    multiprocessing.freeze_support()
+
     ui = UserInterface(process)
     ui.run()
